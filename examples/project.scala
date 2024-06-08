9cafd782dc4992d75b0b0d7fa7bcdb79b43bfa11
Jamie Thompson <bishbashboshjt@gmail.com>
2024-06-08 22:20:07 +0900

    update example

--- examples/project.scala
+++ examples/project.scala
@@ -1,4 +1,4 @@
 //> using scala 3.4.2
-//> using dep io.github.bishabosha::ops-mirror::0.1.0
+//> using dep io.github.bishabosha::ops-mirror::0.1.1
 //> using dep "dev.zio::zio:2.0.21"
 //> using jvm 21
